8f7e00a52d74a02b45de75d4b47237ca34503fa0
Ekaterina210692 <ekaterinavasileva13@gmail.com>
2025-05-13 22:50:24 +0500

    3

--- script.js
+++ script.js
@@ -5,26 +5,26 @@
 // Игра 1
 
 function guessNumber() {
-    const guessRandomNumber = Math.floor(Math.random() * 100) + 1;
-    const operations = ['умножил на 3', 'прибавил 6'];
-    const operationIndex = Math.floor(Math.random() * operations.length);
-    let resultNumber;
+  const guessRandomNumber = Math.floor(Math.random() * 100) + 1;
+  const operations = ['умножил на 3', 'прибавил 6'];
+  const operationIndex = Math.floor(Math.random() * operations.length);
+  let resultNumber;
 
-    if (operationIndex === 0) {
-        resultNumber = guessRandomNumber * 3;
-    } else {
-        resultNumber = guessRandomNumber + 6;
-    }
+  if (operationIndex === 0) {
+    resultNumber = guessRandomNumber * 3;
+  } else {
+    resultNumber = guessRandomNumber + 6;
+  }
 
-    const guess = parseInt(prompt(`Я загадал число, ${operations[operationIndex]} и получил ${resultNumber}. Угадай моё число от 1 до 100`), 10);
+  const guess = parseInt(prompt(`Я загадал число, ${operations[operationIndex]} и получил ${resultNumber}. Угадай моё число от 1 до 100`), 10);
 
-    if (isNaN(guess) || guess < 1 || guess > 100) {
-        console.log("Пожалуйста, введите число от 1 до 100.");
-    } else if (guess === guessRandomNumber) {
-        console.log("Поздравляю, ты угадал!");
-    } else {
-        console.log(`Попробуй ещё раз. Я загадал число ${guessRandomNumber}.`);
-    }
+  if (isNaN(guess) || guess < 1 || guess > 100) {
+    console.log("Пожалуйста, введите число от 1 до 100.");
+  } else if (guess === guessRandomNumber) {
+    console.log("Поздравляю, ты угадал!");
+  } else {
+    console.log(`Попробуй ещё раз. Я загадал число ${guessRandomNumber}.`);
+  }
 }
 console.log(guessNumber());
 
@@ -86,151 +86,139 @@ function playGame() {
   }
 }
 
-// // Задание 1
-// // Дан массив: [1, 5, 4, 10, 0, 3]. Создайте цикл, который будет выводить элементы массива до тех пор, пока не встретит значение 10. После вывода значения 10 в консоль цикл должен прекратить свою работу.
+// Задание 1
 
-// const numbs = [1, 5, 4, 10, 0, 3];
-// for (let i = 0; i < numbs.length; i++) {
-//     console.log(numbs[i]);
-//     if (numbs[i] === 10) {
-//         break;
-//     }
-// }
+const numbs = [1, 5, 4, 10, 0, 3];
+for (let i = 0; i < numbs.length; i++) {
+  console.log(numbs[i]);
+  if (numbs[i] === 10) {
+    break;
+  }
+}
 
-// // Задание 2
-// // Дан массив: [1, 5, 4, 10, 0, 3]. Найдите индекс значения 4 в этом массиве.
+// Задание 2
 
+const num = [1, 5, 4, 10, 0, 3];
+const index = num.indexOf(4);
+console.log(index);
 
-// const num = [1, 5, 4, 10, 0, 3];
-// const index = num.indexOf(4);
-// console.log(index);
+// Задание 3
 
-// // Задание 3
-// // Дан массив чисел: [1, 3, 5, 10, 20]. С помощью метода join выведите элементы массива через пробел (пустую строку ' ').
+let arr = [1, 3, 5, 10, 20];
+arr = arr.join(' ');
+console.log(arr);
 
-// let arr = [1, 3, 5, 10, 20];
-// arr = arr.join(' ');
-// console.log(arr);
+// Задание 4
 
+let multidimensionalArray = [];
 
-// // Задание 4
-// // С помощью вложенных циклов создайте многомерный массив вида: [[1, 1, 1], [1, 1, 1], [1, 1, 1]].
+for (let i = 0; i < 3; i++) {
+  multidimensionalArray[i] = [];
+  for (let j = 0; j < 3; j++) {
+    multidimensionalArray[i][j] = 1;
+  }
+}
 
-// let multidimensionalArray = [];
+console.log(multidimensionalArray);
 
-// for (let i = 0; i < 3; i++) {
-//     multidimensionalArray[i] = [];
-//     for (let j = 0; j < 3; j++) {
-//         multidimensionalArray[i][j] = 1;
-//     }
-// }
+// Задание 5
 
-// console.log(multidimensionalArray);
+let array = [1, 1, 1];
+array.push(2, 2, 2);
+console.log(array);
 
-// // Задание 5
-// // Дан массив: [1, 1, 1]. Добавьте в конец массива значения 2, 2, 2.
+// Задание 6
 
-// let array = [1, 1, 1];
-// array.push(2, 2, 2);
-// console.log(array);
+const sortFilt = [9, 8, 7, 'a', 6, 5];
+sortFilt.sort();
+const filteredArr = sortFilt.filter((item) => item !== 'a');
+console.log(filteredArr);
 
-// // Задание 6
-// // Дан массив: [9, 8, 7, 'a', 6, 5]. С помощью метода sort отсортируйте массив и удалите букву 'a' из массива. Затем выведите массив.
+// Задание 7
 
-// const sortFilt = [9, 8, 7, 'a', 6, 5];
-// sortFilt.sort();
-// const filteredArr = sortFilt.filter((item) => item !== 'a');
-// console.log(filteredArr);
+let guessNumb = [9, 8, 7, 6, 5];
+let numb = prompt('угадай число')
+if (guessNumb.includes(Number(numb))) {
+  alert('«Угадал»');
+} else {
+  alert('«Не угадал»');
 
-// // Задание 7
-// // Дан массив: [9, 8, 7, 6, 5]. Попросите пользователя угадать число с помощью метода prompt. 
-// // Если значение, которое ввел пользователь, есть в массиве, выведите в alert «Угадал», в противном случае — «Не угадал».
+}
 
-// let guessNumb = [9, 8, 7, 6, 5];
-// let numb = prompt('угадай число')
-// if (guessNumb.includes(Number(numb))) {
-//     alert('«Угадал»');
-// } else {
-//     alert('«Не угадал»');
+// Задание 8
 
-// }
+const string = ['abcdef'];
+const reversedString = string[0].split('').reverse().join('');
+console.log(reversedString);
 
-// // Задание 8
-// // Дана строка: 'abcdef'. Выведите в консоль 'fedcba'.
+// Задание 9
 
-// const string = ['abcdef'];
-// const reversedString = string[0].split('').reverse().join('');
-// console.log(reversedString);
+const arra = [[1, 2, 3], [4, 5, 6]];
+const mergedArra = array.flat();
+console.log(mergedArra);
 
-// // Задание 9
-// // Дан массив: [[1, 2, 3],[4, 5, 6]]. Выведите в консоль массив вида: [1, 2, 3, 4, 5, 6].
+// Задание 10
 
-// const arra = [[1, 2, 3], [4, 5, 6]];
-// const mergedArra = array.flat();
-// console.log(mergedArra);
+const newArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (let i = 0; i < newArr.length; i++) {
+  if (newArr[i = 1]) {
+    console.log(newArr[i] + newArr[i + 1]);
+  }
+}
 
-// // Задание 10
+// Задание 11
 
-// const newArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-// for (let i = 0; i < newArr.length; i++) {
-//     if (newArr[i = 1]) {
-//         console.log(newArr[i] + newArr[i + 1]);
-//     }
-// }
+function integers(arr) {
+  return arr.map(item => item ** 2);
+}
 
-// // Задание 11
+console.log(integers([1, 2, 3]));
 
-// function integers(arr) {
-//     return arr.map(item => item ** 2);
-// }
+// Задание 12
 
-// console.log(integers([1, 2, 3]));
+function getLength(arr) {
+  return arr.map(item => item.length);
+}
+console.log(getLength(['добро', 'зло']));
 
-// // Задание 12
+// Задание 13
 
-// function getLength(arr) {
-//     return arr.map(item => item.length);
-// }
-// console.log(getLength(['добро', 'зло']));
+function negativeValues(array) {
+  return array.filter(item => item < 0);
 
-// // Задание 13
+}
+console.log(negativeValues([1, 2, 3, -4, 5, -6, 7, 8, 9, 10]));
 
-// function negativeValues(array) {
-//     return array.filter(item => item < 0);
+// Задание 14 
 
-// }
-// console.log(negativeValues([1, 2, 3, -4, 5, -6, 7, 8, 9, 10]));
+function random() {
+  return Math.floor(Math.random() * 10);
+}
 
-// // Задание 14 
+const arr1 = [];
+for (let i = 0; i < 10; i++) {
+  arr.push(random());
+}
+const evenArr = [];
+for (let i = 0; i < arr1.length; i++) {
+  if (arr1[i] % 2 === 0) {
+    evenArr.push(arr1[i]);
+  };
 
-// function random() {
-//     return Math.floor(Math.random() * 10);
-// }
+}
+console.log(evenArr);
 
-// const arr1 = [];
-// for (let i = 0; i < 10; i++) {
-//     arr.push(random());
-// }
-// const evenArr = [];
-// for (let i = 0; i < arr1.length; i++) {
-//     if (arr1[i] % 2=== 0) {
-//         evenArr.push(arr1[i]);
-//     };
-    
-// }
-// console.log(evenArr);
+// Задание 15 
 
-// // Задание 15 
+function random() {
+  return Math.floor(Math.random() * 10);
+}
 
-// function random() {
-//     return Math.floor(Math.random() * 10);
-// }
+const arr2 = [];
+for (let i = 0; i < 3; i++) {
+  arr.push(random());
+}
 
-// const arr2 = [];
-// for (let i = 0; i < 3; i++) {
-//     arr.push(random());
-// }
-
-// console.log(arr2);
-// console.log(arr2.reduce((a, b) => a + b) / arr2.length);
+console.log(arr2);
+console.log(arr2.reduce((a, b) => a + b) / arr2.length);
 
